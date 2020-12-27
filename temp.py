e9691a939a3a77103a125142bbc8141c668f1c93
Teo777Andrei <69537566+Teo777Andrei@users.noreply.github.com>
2020-12-28 00:20:11 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -8,45 +8,73 @@ import pandas as pd
 
 (_ ,_) ,(test  , _) = cifar10.load_data()
 
+img_to_process = test[1000]
+
+
 model= load_model("final_model.h5") 
+
+
 
 
 kernel_list= model.get_weights()[0]
 
-#image kernel visualisation 
+#image kernels visualisation 
 for image_kernel_iter in range(1 ,33):
     plt.subplot(4 ,8 ,image_kernel_iter)
     plt.imshow(kernel_list[: , :  , 0 , image_kernel_iter-1] ,cmap ="binary")
     
 plt.show()
     
-   
 
-plt.show()
+conv_layers_outputs= [0 , 3] # without activation funtion
 
-outs= [0 , 3]
-outputs= [model.layers[iter].output for iter in outs]
 
+#conv_layers_outputs= [1 , 4] # with ativation funtion (activation maps)
+
+#model to plot feature maps
+
+outputs= [model.layers[iter].output for iter in conv_layers_outputs]
 new_model =Model(inputs = model.inputs ,outputs = outputs) 
 
-to_predict = test[1000].reshape(1 ,32 ,32 ,3)
 
+
+to_predict = img_to_process.reshape(1 ,32 ,32 ,3)
 score = new_model.predict(to_predict)
 
 
+#plot feature maps after first convolution process
 feature_map1 = score[0].reshape(29 ,29 ,32)
 
-for iter in range(1 , 33):
-    plt.subplot(4 , 8 ,iter)
-    plt.imshow(feature_map1[: ,: ,iter-1])
+for conv_layer1_index in range(1 , 33):
+    plt.subplot(4 , 8 ,conv_layer1_index)
+    plt.imshow(feature_map1[: ,: ,conv_layer1_index-1] ,cmap = "binary")
     
 plt.show()
 
+
+#plot feature maps after first convolution process
 feature_map2 =score[1].reshape(15 ,15,16)
 
-for iter in range(1 ,17):
-    plt.subplot(4 ,4 ,iter)
-    plt.imshow(feature_map2[:,: ,iter-1])
+
+for conv_layer2_index in range(1 ,17):
+    plt.subplot(4 ,4 ,conv_layer2_index)
+    plt.imshow(feature_map2[:,: ,conv_layer2_index-1] ,cmap ="binary")
 
 plt.show()
 
+
+#cifar10 datasets categories
+prediction ={0: "airplane" ,
+             1: "automobile" ,
+             2 : "bird" ,
+             3 :"cat" ,
+             4:"deer" ,
+             5:"dog" ,
+             6:"frog" ,
+             7:"horse" ,
+             8:"ship" ,
+             9:"truck" }
+
+final_prediction = model.predict_classes(img_to_process.reshape(1 ,32 ,32 ,3))
+if final_prediction[0] in prediction:
+    print("Algorithm recongnized a %s"  % prediction[final_prediction[0]])
